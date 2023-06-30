ef2767d0476a578424dfc0ce24a636d7d4bdf21c
yagokarlos <127635311+yagokarlos@users.noreply.github.com>
2023-06-30 14:42:35 -0300

    Primeiro commit

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 60
+console.log(`A velocidade do seu carro é ${vel}`)
+if(vel > 60.0){ // condição simples
+    console.log('Você ultrapassou a velocidade maxima, MULTADO!! ')
+}
+console.log('Dirija sempre usando cinto de seguraça')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+let pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){  // condição 
+    console.log('Você é brasileira')
+}else{
+    console.log('Você é estrangeira')
+}

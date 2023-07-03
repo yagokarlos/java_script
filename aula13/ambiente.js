77cfe25dc1239a561d7c3e95bab454a1268ff577
yagokarlos <127635311+yagokarlos@users.noreply.github.com>
2023-07-03 12:12:36 -0300

    Repetições em js com while e do

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+let c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 5)
+
+
+/*
+let c = 1
+while (c <= 3){
+    console.log(`passo ${c}`)
+    c++
+}  
+*/ 
+
+
+
+
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/

2c13861a26b76efa168c8949408276c34eb2d570
yagokarlos <127635311+yagokarlos@users.noreply.github.com>
2023-07-04 13:54:30 -0300

    Contador pronto

--- aula14ex/ex016/scripts.js
+++ aula14ex/ex016/scripts.js
@@ -2,12 +2,18 @@ function contar() {
 const inicio = document.querySelector('#inicio')
 const fim = document.querySelector('#fim')
 const passo = document.querySelector('#passo')
-let res = document.getElementById('resultado')
+const res = document.querySelector('#resultado')
 
-let n1 = Number(inicio.value)
-let n2 = Number(fim.value)
-
-let soma = n1 + n2 
-
-res.innerHTML = `O seu passos vai começar a contar em ${n2}`
-}
+if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0 ) {
+    window.alert('[ERRO] Digite um número válido')
+}else{
+    res.innerHTML = 'Contando: '
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    if(i < f){
+        for (let c = i; c <= f; c += p){
+        res.innerHTML += ` ${c} \u{1F449} `
+    }res.innerHTML += `\u{1F3C1}`
+    }
+}}

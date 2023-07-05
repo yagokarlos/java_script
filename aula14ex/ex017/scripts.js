58b3f4792f0e010219256be449bc76b67d411a42
yagokarlos <127635311+yagokarlos@users.noreply.github.com>
2023-07-05 15:22:10 -0300

    HTML e CSS criado so falta o js

---
+++ aula14ex/ex017/scripts.js
@@ -0,0 +1,10 @@
+/* function gerarTabuada(){
+    const numeroText = document.querySelector('#txtn')
+    let campoTxt = document.querySelector('#txtArea')
+
+    let n1 = Number(campoTxt.value)
+
+    campoTxt.innerHTML = `seu numero é ${n1}`
+
+}
+// txtn */

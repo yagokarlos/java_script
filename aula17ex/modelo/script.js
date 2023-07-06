3c4ed5e51c0c89b1e737ff3bb09232036c1efc5b
yagokarlos <127635311+yagokarlos@users.noreply.github.com>
2023-07-06 18:27:05 -0300

    Projeto terminado

--- aula17ex/modelo/script.js
+++ aula17ex/modelo/script.js
@@ -1,22 +1,41 @@
 // pegando os inputs e os button
 const txtn = document.querySelector('#txtn');
 const btnAdd = document.querySelector('#btn');
-const txtArea = document.querySelector('#txtArea');
+const lista = document.querySelector('#txtArea');
 const btnFinalizar = document.querySelector('#btn1');
+const res = document.querySelector('#resultado')
+let valores = []
 
 // Adicionando os eventos nos button
 btnAdd.addEventListener('click', clickadd );
-btnFinalizar.addEventListener('click' , btnFinalizar );
+btnFinalizar.addEventListener('click' , finalizar );
 
 // função de adicionar o numero na caixa de texto
 function clickadd(){
-    if(txtn.value == 0 || txtn.value >= 101 ){
+    if(txtn.value <= 0 || txtn.value >= 101){
         window.alert('Digite um número que seja válido')
                 
     }else{
         let n1 = Number(txtn.value)
         let iten = document.createElement('option')
         iten.text = `valor ${n1} adicionado`
-        txtArea.appendChild(iten)
+        lista.appendChild(iten)
+        valores.push(Number(n1))
+        res.innerHTML = ""
+    }
+
+    txtn.value = ""
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar')
+    }else{
+        let total = valores.length
+
+        // lista.innerHTML = ""
+        let maior = valores[0]
+        let menor = valores[0]
+        res.innerHTML = `<p> Ao todo, temos ${total} numeros cadastrados </p>`
     }
 }
